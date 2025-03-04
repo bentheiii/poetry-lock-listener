11b8c35c027d8aabba7fe5164003b7871527df9d
Ben Avrahami <wouldntyouliketoknow@weather.boy>
2025-03-04 18:41:38 +0200

    lint

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,16 +1,19 @@
 import json
+
 from pytest import fixture
-from yellowbox.image_build import build_image
 from yellowbox.containers import create_and_pull, removing
+from yellowbox.image_build import build_image
+
 
 @fixture(scope="module", params=["2.*", "1.*"])
 def poetry_version(request):
     return request.param
 
+
 @fixture(scope="module")
 def image(poetry_version, docker_client):
     image_name = f"test-project:{poetry_version}".replace(".*", "")
-    
+
     with build_image(
         docker_client,
         image_name,
@@ -19,6 +22,7 @@ def image(poetry_version, docker_client):
         buildargs={"POETRY_VERSION": poetry_version},
     ) as image:
         yield image
+
 
 def test_update(docker_client, image):
     cntr = create_and_pull(docker_client, image, "poetry add types-retry@latest")
@@ -32,6 +36,6 @@ def test_update(docker_client, image):
         _exec, raw_change, raw_context = args
         changes = json.loads(raw_change)
         context = json.loads(raw_context)
-        change, = changes
+        (change,) = changes
         assert change["package"] == "types-retry"
         assert context == {}
